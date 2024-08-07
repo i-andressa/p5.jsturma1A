59998408c4017f9150ef74485dca593f48aaddf9
i-andressa <andressa.iarochinski@escola.pr.gov.br>
2024-08-07 14:27:18 -0300

    Add files via upload
    É um jogo pong no pj5

---
+++ sketch.js
@@ -0,0 +1,75 @@
+//variáveis da bolinha 
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 22;
+let raio = diametro / 2;
+
+//velocidade da bolinha 
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+ 
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+let colidiu = false;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaMinhaRaquete();
+  // verificaColisaoRaquete();
+}
+
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+} 
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+  
+function verificaColisaoBorda() {
+   if (xBolinha + raio > width || xBolinha - raio < 0) {
+      velocidadeXBolinha *= -1;
+  }
+   if (yBolinha + raio > height || yBolinha - raio < 0) {
+       velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete () {
+  rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+  
+function movimentaMinhaRaquete() {
+  if(keyIsDown(UP_ARROW)) {
+    yRaquete-=10;
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    yRaquete+= 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete) {
+   velocidadeXBolinha *= -1
+  }
+}
+ 
+function novaColisao(){
+  colidiu = collideRectCircle(xRaquete, yRaquete, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+  if(colidiu) {
+    velocidadeXBolinha *= -1;
+  }
+}
